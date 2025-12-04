b2bc50dc2dac78fa71bb473c7583c6308207957c
Le Cong Thien Bao <thienbao@Les-MacBook-Air.local>
2025-12-04 12:01:22 +0700

    styling expenses Summary

--- components/ExpensesOutput/ExpensesSummary.tsx
+++ components/ExpensesOutput/ExpensesSummary.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { GlobalStyles } from "@/constants/styles";
+import { StyleSheet, Text, View } from "react-native";
 import { Expense, Expenses } from "./ExpensesOutput";
 type ExpensesSummaryProp = {
     priodName:string,
@@ -11,10 +12,31 @@ function ExpensesSummary ({priodName, expenses}:ExpensesSummaryProp){
     },0)
 
     return (
-        <View>
-            <Text>{priodName}</Text>
-            <Text>${expensesSum.toFixed(2)}</Text>
+        <View style={styles.container}>
+            <Text style={styles.period}>{priodName}</Text>
+            <Text style={styles.sum}>${expensesSum.toFixed(2)}</Text>
         </View>
     )
 }
 export default ExpensesSummary;
+
+const styles = StyleSheet.create({
+    container:{
+        padding:8,
+        backgroundColor: GlobalStyles.colors.primary50,
+        borderRadius:6,
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center'
+    },
+    period:{
+        fontSize:16,
+        color: GlobalStyles.colors.primary400
+    },
+    sum:{
+        fontSize:16,
+        fontWeight:'bold',
+        color: GlobalStyles.colors.primary500
+    }
+
+})
