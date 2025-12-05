5bcb40f269a24df6158f51a2dab114d015987cd8
Le Cong Thien Bao <thienbao@Les-MacBook-Air.local>
2025-12-05 13:58:48 +0700

    add logic function add, remove, update + share via context provider

--- store/Expenses-context.tsx
+++ store/Expenses-context.tsx
@@ -2,13 +2,13 @@ import { Expense } from "@/components/ExpensesOutput/ExpensesOutput";
 import { createContext, ReactNode, useReducer } from "react";
 
 type updateExpense = {
-  decription: string;
+  description: string;
   date: Date;
   amount: number;
 };
 
 type AddExpenseParams = {
-  decription: string;
+  description: string;
   date: Date;
   amount: number;
 };
@@ -22,18 +22,16 @@ type updateExpenseParams = {
 
 type ExpensesContextType = {
   expenses: Expense[];
-  addExpense?: ({ decription, date, amount }: AddExpenseParams) => void;
-  deleteExpense?: ({ id }: DeleteExpenseParams) => void;
-  updateExpense?: ({ id, expenseData }: updateExpenseParams) => void;
+  addExpense: ({ description, date, amount }: AddExpenseParams) => void;
+  deleteExpense: ({ id }: DeleteExpenseParams) => void;
+  updateExpense: ({ id, expenseData }: updateExpenseParams) => void;
 };
 
 type ExpensesContextProviderProp = {
   children: ReactNode;
 };
 
-type state = {
-    expenses:Expense[]
-}
+type state = Expense[];
 type Action =
   | { type: "ADD"; payload: AddExpenseParams }
   | { type: "REMOVE"; payload: { id: string } }
@@ -41,41 +39,47 @@ type Action =
 
 export const ExpensesContext = createContext<ExpensesContextType>({
   expenses: [],
-  addExpense: ({ decription, date, amount }) => {},
-  deleteExpense: ({id}) => {},
-  updateExpense: ({id,expenseData}) => {},
+  addExpense: ({ description, date, amount }) => {},
+  deleteExpense: ({ id }) => {},
+  updateExpense: ({ id, expenseData }) => {},
 });
 
-const initialState: state = {
-  expenses: [],
-};
+const initialState: state = [];
 
-function expensesReducer(state:state,action:Action){
-    switch (action.type) {
-        case "ADD":
-        case "REMOVE":
-        case "UPDATE":
-        default:
-            return state
-    }
+function expensesReducer(state: state, action: Action) {
+  switch (action.type) {
+    case "ADD":
+      const id = new Date().toString() + Math.random().toString();
+      return [{ id: id, ...action.payload }, ...state];
+    case "REMOVE":
+      return state.filter((exp) => exp.id !== action.payload.id);
+    case "UPDATE":
+      return state.map((exp) =>
+        exp.id === action.payload.id
+          ? { ...exp, ...action.payload.expenseData }
+          : exp
+      );
+    default:
+      return state;
+  }
 }
 
-
-
 function ExpensesContextProvider({ children }: ExpensesContextProviderProp) {
-  const [expenses,dispatch] = useReducer(expensesReducer,initialState);
-  function addExpense (expenseData:AddExpenseParams) {
-    dispatch({type:"ADD",payload:expenseData})
+  const [expenses, dispatch] = useReducer(expensesReducer, initialState);
+  function addExpense(expenseData: AddExpenseParams) {
+    dispatch({ type: "ADD", payload: expenseData });
   }
-  function deleteExpense(id:DeleteExpenseParams){
-    dispatch({type:"REMOVE",payload:id})
+  function deleteExpense(id: DeleteExpenseParams) {
+    dispatch({ type: "REMOVE", payload: id });
   }
-  function updateExpense({id,expenseData}:updateExpenseParams){
-    dispatch({type:"UPDATE",payload:{id,expenseData}})
+  function updateExpense({ id, expenseData }: updateExpenseParams) {
+    dispatch({ type: "UPDATE", payload: { id, expenseData } });
   }
 
-  return (<ExpensesContext.Provider value={expenses}>
-    {children}
-  </ExpensesContext.Provider>)
+  return (
+    <ExpensesContext.Provider value={{ expenses, addExpense,deleteExpense,updateExpense}}>
+      {children}
+    </ExpensesContext.Provider>
+  );
 }
 export default ExpensesContextProvider;
