a9c24772031f5b14017091fd0a024cbdd3908756
Le Cong Thien Bao <thienbao@Les-MacBook-Air.local>
2025-12-05 15:08:26 +0700

    filter recent expense

--- screens/RecentExpenses.tsx
+++ screens/RecentExpenses.tsx
@@ -1,10 +1,19 @@
 import ExpensesOutput from "@/components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "@/store/Expenses-context";
+import { getDateMinusDays } from "@/util/date";
 import { useContext } from "react";
 
 function RecentExpenses() {
 
   const expensesCt = useContext(ExpensesContext)
-  return <ExpensesOutput expensePeriod="Last 7 Day" expenses={expensesCt.expenses} />;
+
+  const recenntExpenses = expensesCt.expenses.filter((exp) => {
+    const today = new Date();
+    const date7DaysAgo = getDateMinusDays(today,7)
+    return exp.date > date7DaysAgo;
+  });
+
+
+  return <ExpensesOutput expensePeriod="Last 7 Day" expenses={recenntExpenses} />;
 }
 export default RecentExpenses;
